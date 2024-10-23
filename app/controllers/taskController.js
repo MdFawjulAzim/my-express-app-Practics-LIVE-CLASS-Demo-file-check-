a9b0aa9cd74453260a1cab88dbcdfac444cc4770
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-24 03:20:31 +0600

    demo 2 done

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -12,4 +12,22 @@ export const demo1= async (req,res)=>{
     return res.json({message:name,city:city,age:age});
 }
 
+//demo 1 query string
 
+export const demo1q= async (req,res)=>{
+
+    let country=req.query.country;
+    let lang=req.query.lang;
+    
+
+    return res.json({message:country,langague:lang});
+}
+
+//demo 2
+
+export const demo2= async (req,res)=>{
+
+    let data=req.body['city'];
+
+    return res.json({message:data});
+}
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,13 @@ import * as taskController from "../app/controllers/taskController.js";
 //demo 1 url params 
 router.post("/demo1/:name/:age/:city",taskController.demo1);
 
+//demo 1 query params
+router.post("/demo1q/:name/:age/:city",taskController.demo1q);
+
+//demo 2 Request body params
+router.post("/demo2",taskController.demo2);
+
+
 
 
 
